3490ca22c82259ee60b5f118201f8e1c24a13e20
uc-asa <developerashu7695@gmail.com>
2022-05-18 14:32:38 +0530

    fix: theme issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import theme from "./theme";
-import { ThemeProvider } from "@material-ui/core/styles";
 // import reportWebVitals from "./reportWebVitals";
 import WithTheme from "./containers/oneTimePayment/withTheme";
-import SetupPaymentPlan from "./containers/setupPaymentPlan";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
       <div>
         <WithTheme />
       </div>
-    {/* </ThemeProvider> */}
   </React.StrictMode>,
 
   document.getElementById("root")
