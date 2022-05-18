6d8173aa536b5c608df6c4abbdfef0c2deed2632
uc-asa <developerashu7695@gmail.com>
2022-05-18 14:53:30 +0530

    chore: added containers

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-export { default as Button } from "./src/components/ui/Button";
+export { default as Button } from "./src/components/Button";
 export { default as TestLink } from "./src//components/ui/TestLink";
+
+// Containers
+
+export { default as OneTimePayment } from "./src/containers/oneTimePayment";
